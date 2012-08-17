2e28f88e54f42e01dce1d94d9b1af0844d01e673
Eric <mariole@keller.re>
2012-08-17 10:49:57 +0200

    update script automatically making the links to the configuration files

--- script/apply_rc.sh
+++ script/apply_rc.sh
@@ -8,11 +8,21 @@ SCRIPTDIR=`dirname $0`
 # make sure the script is always running from the same directory
 pushd $SCRIPTDIR > /dev/null
 
-DOT_CONFIGURATION=`find ../ -iname ".*rc"`
+CONFIG_DIR=`dirname $(pwd)`
+echo "CONFIG_DIR: ${CONFIG_DIR}"
+DOT_CONFIGURATION=`find ${CONFIG_DIR} -type f \( -iname ".*rc" -o -iname ".*conf" \)`
 echo "DOT_CONFIGURATION: ${DOT_CONFIGURATION}"
 for config in ${DOT_CONFIGURATION};
 do
+	CONFIG_NAME=`basename ${config}`
 	echo "configuration: ${config}"
+	if [ -f ${HOME}/${CONFIG_NAME} ] 
+	then
+		echo "configuration already exists..."
+	else
+		echo "linking configuration..."
+		ln -s ${config} ~/${CONFIG_NAME}
+	fi
 done
 
 popd > /dev/null
