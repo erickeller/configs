9ba67557ab569111ae0760ff1d1da60d0a641bb3
Eric Keller <keller.eric@gmail.com>
2021-12-11 22:49:09 +0100

    rework low_battery script notification and suspend
    display message when battery is discharging and less than 15%
    suspend when battery is discharging and less than 5%

--- script/low_battery.sh
+++ script/low_battery.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
+BATTINFO=`/usr/bin/acpi -b`
+# suspend after notification
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | sed -e 's/%,//'` -lt 5 ]]
+then
+    DISPLAY=:0.0 /usr/bin/notify-send --urgency=critical "suspend" "$BATTINFO"
+    sleep 30
+    /usr/bin/i3lock && systemctl suspend
+fi
+# notify when battery is discharging and less than 15%
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | sed -e 's/%,//'` -lt 15 ]]
+then
     DISPLAY=:0.0 /usr/bin/notify-send "low battery" "$BATTINFO"
 fi
+
