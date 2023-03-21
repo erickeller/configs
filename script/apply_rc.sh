b847d579ae40b761d4f21082d56da6d8697d6c26
Eric Keller <eric.keller.ek1@roche.com>
2023-03-21 12:44:25 +0000

    fixup how docker is installed

--- script/apply_rc.sh
+++ script/apply_rc.sh
@@ -97,10 +97,12 @@ EOF
 install_docker()
 {
     sudo sh -c 'apt-get install -y -qq apt-transport-https ca-certificates curl >/dev/null'
-    sudo sh -c 'curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | apt-key add -qq - >/dev/null'
-    sudo sh -c "echo 'deb [arch=amd64] https://download.docker.com/linux/ubuntu ${UBUNTU_CODENAME} edge' > /etc/apt/sources.list.d/docker.list"
+    sudo sh -c 'sudo apt-get remove docker docker-engine docker.io containerd runc'
+    sudo mkdir -m 0755 -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo sh -c "apt-get update -qq >/dev/null"
-    sudo sh -c "apt-get install -y -qq --no-install-recommends docker-ce >/dev/null"
+    sudo apt-get install -qq -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null
 }
 
 install_lxd()
